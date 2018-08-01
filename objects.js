6d270c1fc29551d139888733022d8e0ecbde1aa4
paul@paulleach.co.uk <paul@paulleach.co.uk>
2018-08-01 11:56:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,11 +1,11 @@
 var playlist = { Keane: 'Everybody\'s Changing'};
 
 function updatePlaylist(list, artist, song) {
-  list.artist = song;
+  list[artist] = song;
   return list;
 }
 
 function removeFromPlaylist(list, artist) {
-  delete list.artist;
+  delete list[artist];
   return list;
 }
